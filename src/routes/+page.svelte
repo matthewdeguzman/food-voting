e69b65b6f6b96d8c65cb2e5efb676714154a31bf
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-11-19 14:20:15 -0500

    ui fix for review

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,58 +23,52 @@
     </title>
 </svelte:head>
 
-<div class="grid h-screen grid-cols-[auto_350px_auto] bg-black text-white">
+<div class="grid h-screen grid-cols-[auto_350px_auto] bg-black text-white mt-8">
 	<div class="col-start-2 flex h-full items-center">
-		{#if !formResponse}
-			<form class="flex flex-col gap-6" method="POST">
-				<h1 class="text-center text-4xl font-bold">Final Review Snacks</h1>
-				<p class="mb-6 text-center">
-					Please select your desired snack for the final exam review this Wednesday. If you cannot
-					eat any/don't like the available options, please enter a snack in the other category!
-				</p>
-				{#each categories as { name, emoji }}
-					<Category {name} {emoji} selected={name === $selectedCategory} />
-				{/each}
+		<form class="flex flex-col gap-6" method="POST">
+			<h1 class="text-center text-4xl font-bold">Final Review Snacks</h1>
+			<p class="mb-6 text-center">
+				Please select your desired snack for the final exam review this Wednesday. If you cannot
+				eat any/don't like the available options, please enter a snack in the other category!
+			</p>
+			{#each categories as { name, emoji }}
+				<Category {name} {emoji} selected={name === $selectedCategory} />
+			{/each}
 
-				<input
-					type="text"
-					name="category"
-					id="category"
-					class="hidden"
-					bind:value={$selectedCategory}
-				/>
+			<input
+				type="text"
+				name="category"
+				id="category"
+				class="hidden"
+				bind:value={$selectedCategory}
+			/>
 
-				<div class="mt-4 flex flex-col gap-4">
-					<div class="flex items-center gap-4">
-						<Checkbox id="other" bind:checked={otherChecked} />
-						<Label
-							for="other"
-							class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-							>Add other category</Label
-						>
-					</div>
-					<input
-						class:invisible={!otherChecked}
-						type="text"
-						oninput={(e) => {
-							selectedCategory.set(e.target.value);
-						}}
-						class="w-full rounded-2xl border border-solid border-white bg-transparent px-4 py-2 text-white"
-						placeholder="Other"
-					/>
-				</div>
-
-				<div class:invisible={$selectedCategory === ''} class="mt-8 flex flex-col items-center">
-					<button
-						class="w-fit rounded-full border border-solid border-white px-6 py-2 hover:bg-white hover:text-black"
-						>Submit</button
+			<div class="mt-4 flex flex-col gap-4">
+				<div class="flex items-center gap-4">
+					<Checkbox id="other" bind:checked={otherChecked} />
+					<Label
+						for="other"
+						class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+						>Add other category</Label
 					>
 				</div>
-			</form>
-		{:else if formResponse?.success}
-			<p class="mt-4 text-green-500">success!</p>
-		{:else}
-			<p class="mt-4 text-red-500">{formResponse.message}</p>
-		{/if}
+				<input
+					class:invisible={!otherChecked}
+					type="text"
+					oninput={(e) => {
+						selectedCategory.set(e.target.value);
+					}}
+					class="w-full rounded-2xl border border-solid border-white bg-transparent px-4 py-2 text-white"
+					placeholder="Other"
+				/>
+			</div>
+
+			<div class:invisible={$selectedCategory === ''} class="mt-8 flex flex-col items-center">
+				<button
+					class="w-fit rounded-full border border-solid border-white px-6 py-2 hover:bg-white hover:text-black"
+					>Submit</button
+				>
+			</div>
+		</form>
 	</div>
 </div>
